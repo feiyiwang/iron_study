cb31fbdbbc53cf71574dac570a93d6f2ff253d9d
feiyiwang <rephycca@gmail.com>
2022-03-25 00:24:58 +0200

    create outcome matrix using PheWAS

--- analysis.py
+++ analysis.py
@@ -13,8 +13,6 @@ sex_path = '/finngen/library-red/finngen_R9/phenotype_1.0/data/finngen_R9_minimu
 snp_path = '/finngen/green/FeiyiWang/all_variants_and_proxies.csv'
 # source: R package - PheWAS
 phecode_path = '/finngen/green/FeiyiWang/phecode_map.csv'
-# source: https://phewascatalog.org/phecodes_icd10
-phecode_path2 = '/finngen/green/FeiyiWang/phecode_icd10.csv'
 # source: https://phewascatalog.org/files/phecode_definitions1.2.csv.zip
 phecode_path3 = '/finngen/green/FeiyiWang/phecode_sex.csv'
 
@@ -70,7 +68,7 @@ for i in tqdm.tqdm(range(len(snp_list))):
 # 2459 cols -> 1 finngen_id + 2458 SNPs
 exposure_matrix.to_csv('exposure_matrix.csv', index=None)
 # delete dataframes to release some memory
-del ped, freq, snp, demo
+del ped, freq, snp
 
 
 # Build a matrix of confounding
@@ -94,129 +92,30 @@ confounding = confounding.merge(demo[['finngen_id', 'age']], 'left')
 # 13 cols -> 1 finngen_id + 1 sex + 10 principal components + 1 age
 confounding.to_csv('confounding.csv', index=None)
 # delete dataframes to release some memory
-del pca
+del pca, demo
 
 
 # Convert ICD codes to a list of phecodes
-# 1. clean the event data
 # load data
 events = pd.read_csv(event_path, sep='\t')
 # remove irrelevant codes
-events = events[~events.SOURCE.isin(['OPER_OUT', 'OPER_IN'])]
 events = events[(events.CATEGORY.str.startswith('ICD')) | (events.CATEGORY.str.match('\d+'))]
 # keep only icd9 and icd10
-events = events[events.ICDVER.isin(['9', '10'])][['FINNGENID', 'SOURCE', 'EVENT_AGE', 'CODE1']]
+events = events[events.ICDVER.isin(['9', '10'])][['FINNGENID', 'SOURCE', 'CODE1', 'ICDVER']]
 # remove n/a value
 events = events[~events.CODE1.isna()]
-# remove duplicates
-events = events[~events.duplicated(subset=['FINNGENID', 'CODE1'])]
-# split the data by icd version
-events_icd9 = events[events.ICDVER == '9'][['FINNGENID', 'EVENT_AGE', 'CODE1']]
-events_icd10 = events[events.ICDVER == '10'][['FINNGENID', 'EVENT_AGE', 'CODE1']]
-# replace codes ending with lower case with upper case
-to_fix = events_icd9[events_icd9.CODE1.str[-1].isin(['a', 'b', 'x'])].CODE1
-for i, item in to_fix.iteritems():
-    events_icd9.loc[i, 'CODE1'] = item.upper()
-
-# 2. process the mapping data
-# load data
-phecode_map = pd.read_csv(phecode_path)
-phecode_map2 = pd.read_csv(phecode_path2)  # icd 10 only
-phecode_map2 = phecode_map2.rename(columns={'ICD10': 'code', 'PheCode': 'phecode'})
-# 1) process the icd 9 mapping data
-icd9 = phecode_map[phecode_map.vocabulary_id == 'ICD9CM'][['code', 'phecode']]
-icd9['icd'] = icd9.code.str.replace('.', '')  # remove dot from the codes
-icd9['sign'] = get_signs(icd9)
-# 2) process the icd 10 mapping data
-icd10 = phecode_map[phecode_map.vocabulary_id == 'ICD10CM'][['code', 'phecode']]
-# some codes in icd10 contain '-'  e.g. A00-A09, T51-T65
-icd10_ = icd10[icd10.code.str.contains('-')]  # len = 121
-map_dict = {}
-for i, row in icd10_.iterrows():
-    code_type = row.code[0]
-    code_range = [int(i[1:]) for i in row.code.split('-')]
-    code_list = [str(i) for i in list(range(code_range[0], code_range[1] + 1))]
-    code_list = ['0' + i if len(i) == 1 else i for i in code_list]
-    for num in code_list:
-        map_dict[code_type + num] = row.phecode
-icd10_ = pd.DataFrame({'code': map_dict.keys(), 'phecode': map_dict.values()})
-icd10 = pd.concat([icd10, icd10_, phecode_map2])
-icd10 = icd10[(~icd10.phecode.isna()) & (~icd10.code.str.contains('-'))]
-icd10 = icd10[~icd10.duplicated()]
-icd10['icd'] = icd10.code.str.replace('.', '')
-icd10['sign'] = get_signs(icd10)
-# split to two parts by checking duplicates - one phecode to many icd codes
-dup_list = icd10[icd10.icd.duplicated()].icd.tolist()
-icd10_dup = icd10[icd10.code.isin(dup_list)]  # len = 8110  7457
-icd10 = icd10[~icd10.code.isin(dup_list)]  # len = 66722  66909
-icd10_dup = icd10_dup.sort_values(by='icd')
-icd10 = icd10.sort_values(by='icd')
-
-# 3. ICD 9 mapping
-# 1) exact mapping
-events_icd9 = events_icd9.merge(icd9[['icd', 'phecode']], 'left', left_on='CODE1', right_on='icd')
-print_result(events_icd9)  # 0.0%
-# 2) mapping by the first 4 elements
-# if all the codes with the same first 4 elements have the same phecode
-icd9_ = icd9[~icd9.sign.isin([0, 1])][['phecode', 'sign']]
-icd9_ = icd9[~icd9_.duplicated()]
-events_icd9_ = get_events_subset(events_icd9)
-events_icd9_ = events_icd9_.merge(icd9[['sign', 'phecode']], 'left', left_on='code_', right_on='sign')
-events_icd9_ = events_icd9_[~events_icd9_.phecode.isna()]
-events_icd9.loc[events_icd9_['index'].tolist(), 'phecode'] = events_icd9_.phecode.tolist()
-print_result(events_icd9)  # 29.2%
-# if these 5-digit Finnish codes can be converted to standardized 4-digit codes only
-events_icd9_ = get_events_subset(events_icd9)
-events_icd9_['phecode'] = get_phecodes(events_icd9_, icd9)
-events_icd9_ = events_icd9_[~events_icd9_.phecode.isin(['fail', 'na'])]
-events_icd9.loc[events_icd9_['index'].tolist(), 'phecode'] = events_icd9_.phecode.tolist()
-print_result(events_icd9)  # 68.7%
-
-# 4. ICD 10 mapping
-events_icd10_dup = events_icd10[events_icd10.CODE1.isin(dup_list)]
-events_icd10 = events_icd10[~events_icd10.CODE1.isin(dup_list)]
-''' For events_icd10 '''
-# 1) exact mapping
-# if each icd code is mapped to only one phecode
-events_icd10 = events_icd10.merge(icd10[['icd', 'phecode']], 'left', left_on='CODE1', right_on='icd')
-print_result(events_icd10)  # 77.2%
-# if the code if from those A00-A09, T51-T65, etc.
-events_icd10_ = events_icd10[events_icd10.phecode.isna()]
-events_icd10_['code_'] = events_icd10_.CODE1.str[:3]
-events_icd10_ = events_icd10_[['FINNGENID', 'code_']]
-events_icd10_['index'] = events_icd10_.index
-events_icd10_ = events_icd10_.merge(icd10_, 'left', left_on='code_', right_on='code')
-events_icd10_ = events_icd10_[~events_icd10_.phecode.isna()]
-events_icd10.loc[events_icd10_['index'].tolist(), 'phecode'] = events_icd10_.phecode.tolist()
-print_result(events_icd10)  # 82.3%
-# 2) mapping by the first n-1 elements in the code
-# e.g. Given O9980 in Finngen, search O998 and check if all codes begin with O998 are mapped to the same phecode
-events_icd10_ = events_icd10[events_icd10.phecode.isna()][['FINNGENID', 'CODE1']]
-events_icd10_['index'] = events_icd10_.index
-code = events_icd10_.CODE1.unique().tolist()
-phecode = [get_phecodes(i[:-1], icd10) for i in tqdm.tqdm(code)]
-events_icd10_ = events_icd10_.merge(pd.DataFrame({'CODE1':code, 'phecode':phecode}), 'left')
-events_icd10_ = events_icd10_[~events_icd10_.phecode.isin('?')]
-events_icd10.loc[events_icd10_['index'].tolist(), 'phecode'] = events_icd10_.phecode.tolist()
-print_result(events_icd10)  # 91.0%
-events_icd10 = events_icd10[['FINNGENID', 'CODE1', 'phecode']]
-''' For events_icd10_dup '''
-# exact mapping
-# merge a new dataset to concat the events_icd10
-events_icd10_ = events_icd10_dup[~events_icd10_dup.duplicated()][['FINNGENID', 'CODE1']]
-events_icd10_ = events_icd10_.merge(icd10_dup[['icd', 'phecode']], left_on='CODE1', right_on='icd')
-events_icd10 = pd.concat([events_icd10, events_icd10_], axis=0)
-print_result(events_icd10)  # 92.9%
-
-# 5. merge events and clean the data
-# icd10 accounts for 95.95% of all the codes in events
-events = pd.concat([events_icd10, events_icd9], axis=0)
-print_result(events) # 92.0%
+# add cols to events according to createPhenotypes requirements
+events['vocabulary_id'] = np.select([(events.ICDVER == '9'), (events.ICDVER == '10')], ['ICD9CM', 'ICD10CM'])
+events = events.rename(columns={'FINNGENID': 'id', 'CODE1': 'code'})
+events['count1'] = 1
+events = events[['id', 'vocabulary_id', 'code', 'count1']]
 events.to_csv('events.csv', index=None)
-# get a list of unique phecodes for modeling
-phecode_list = sorted(phecode_map.phecode.unique().tolist())
-events = events[events.phecode.isin(phecode_list)][['FINNGENID', 'phecode']]
-events = events.rename(columns={'FINNGENID':'finngen_id'})
+# create a sex_df for createPhenotype() in R
+sex_df = confounding[confounding.sex != ''][['finngen_id', 'sex']]
+sex_df['sex'] = np.select([(sex_df.sex == 1), (sex_df.sex == 0)], ['M', 'F'])
+sex_df = sex_df.rename(columns={'finngen_id': 'id'})
+sex_df.to_csv('sex_df.csv', index=None)
+# the rest will be done in R
 
 
 # Convert events to a matrix of outcomes
@@ -243,6 +142,7 @@ del events
 phecode_path3 = '/finngen/green/FeiyiWang/phecode_sex.csv'
 phecode_sex = pd.read_csv(phecode_path3) # 166 sex-specified
 phecode_def = pd.DataFrame({'phecode':phecode_list, 'n_cases':n_cases})
+# remove phecodes that has fewer than 100 individuals
 phecode_def = phecode_def[phecode_def.n_cases >= 100]
 phecode_def = phecode_def.merge(phecode_sex, 'left') # 147 sex-specified
 phecode_def['sex'] = np.select([(phecode_def.sex == 'Female'),
@@ -316,5 +216,56 @@ for snp in tqdm.tqdm(exposure_matrix.columns[67:68]):
 end = datetime.datetime.now()
 print(end - start)
 # 1:02:29 for one snp
+
+
+from scipy.stats import uniform
+from scipy.stats import randint
+import numpy as np
+import matplotlib.pyplot as plt
+
+# sample data
+df = pd.DataFrame({'gene' : ['gene-%i' % i for i in np.arange(10000)],
+'pvalue' : uniform.rvs(size=10000),
+'chromosome' : ['ch-%i' % i for i in randint.rvs(0,12,size=10000)]})
+
+# -log_10(pvalue)
+df['minuslog10pvalue'] = -np.log10(df.pvalue)
+df.chromosome = df.chromosome.astype('category')
+df.chromosome = df.chromosome.cat.set_categories(['ch-%i' % i for i in range(12)], ordered=True)
+df = df.sort_values('chromosome')
+
+# How to plot gene vs. -log10(pvalue) and colour it by chromosome?
+df['ind'] = range(len(df))
+df_grouped = df.groupby(('chromosome'))
+
+# manhattan plot
+fig = plt.figure(figsize=(14, 8)) # Set the figure size
+ax = fig.add_subplot(111)
+colors = ['darkred','darkgreen','darkblue', 'gold']
+x_labels = []
+x_labels_pos = []
+for num, (name, group) in enumerate(df_grouped):
+    group.plot(kind='scatter', x='ind', y='minuslog10pvalue',color=colors[num % len(colors)], ax=ax)
+    x_labels.append(name)
+    x_labels_pos.append((group['ind'].iloc[-1] - (group['ind'].iloc[-1] - group['ind'].iloc[0])/2))
+ax.set_xticks(x_labels_pos)
+ax.set_xticklabels(x_labels)
+
+# set axis limits
+ax.set_xlim([0, len(df)])
+ax.set_ylim([0, 3.5])
+
+# x axis label
+ax.set_xlabel('Chromosome')
+
+# show the graph
+plt.show()
+
+# fam id
+# robust error
+#
+# gene name vs rsid
+
+
 
 
